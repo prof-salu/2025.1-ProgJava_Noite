7be3a5fb8f157773772d15aa623f7318fc8f7a34
Salustiano Oliveira <saluorodrigues@gmail.com>
2025-03-19 23:06:54 -0300

    aula02

---
+++ aula02/src/aula02/OlaMundo.java
@@ -0,0 +1,11 @@
+package aula02;
+
+public class OlaMundo {
+	public static void main(String args[]) {
+		System.out.println("Olá mundo!");
+		int idade;
+		String nome;
+	}
+}
+
+//bytecode -> pode ser considerado o .exe do java
